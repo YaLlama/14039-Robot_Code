c7572b7d5cfdc5dae2f4375e2ecef4d26cf3c78c
Rohan B <rohanbandaru1@gmail.com>
2019-07-14 15:24:44 -0400

    First Commit
    Commit the existing android studio project

---
+++ RobotController20190714/README.md
@@ -0,0 +1,4 @@
+# 14039-IrRaTiONAl-Robot-Code
+
+This is FTC team 14039s robot code!
+Please push to your own branch and then I'll merge with the master branch.
---
+++ RobotController20190714/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystem.java
@@ -0,0 +1,9 @@
+package org.firstinspires.ftc.teamcode;
+
+public abstract class Subsystem {
+
+    public abstract boolean isRunning();
+
+    public abstract void doAction(String action);
+
+}
