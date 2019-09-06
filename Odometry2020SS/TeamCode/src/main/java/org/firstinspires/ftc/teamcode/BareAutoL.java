e6d46d318823b928157775f4d49c55421ecff42a
Rohan B <rohanbandaru1@gmail.com>
2019-09-06 17:59:13 -0400

    OpModeTemplates

---
+++ Odometry2020SS/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BareAutoL.java
@@ -0,0 +1,41 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+
+import org.firstinspires.ftc.teamcode.Controllers.ConstantP;
+
+
+@Autonomous(name="Bare Bones Auto", group="Linear Opmode")
+
+public class BareAutoL extends LinearOpMode {
+
+    // Declare OpMode members.
+
+
+    public void doAction(Subsystem s, String action){
+        while(s.isRunning()){
+            s.doAction(action);
+        }
+    }
+
+
+    private void initialize(){
+        // Initialize all objects declared above
+
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+    }
+
+
+    @Override
+    public void runOpMode() {
+        // Wait for the game to start (driver presses PLAY)
+        initialize();
+        waitForStart();
+
+        //Make sure nothing is still using the thread
+    }
+}
---
+++ Odometry2020SS/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BareTeleOpL.java
@@ -0,0 +1,36 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+
+@TeleOp(name="Bare Teleop", group="Linear Opmode")
+
+public class BareTeleOpL extends LinearOpMode {
+
+    // Declare OpMode members.
+
+
+    private void initialize(){
+        // Initialize all objects declared above
+
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+    }
+
+
+    @Override
+    public void runOpMode() {
+        // Wait for the game to start (driver presses PLAY)
+        initialize();
+        waitForStart();
+
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+            
+            telemetry.update();
+        }
+    }
+}
