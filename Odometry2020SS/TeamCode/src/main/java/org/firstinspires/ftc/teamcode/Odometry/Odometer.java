56fe59c12b7444636eeec9d8ba14c24866a6f6f6
14039 <36121698+14039@users.noreply.github.com>
2019-09-12 15:20:47 -0400

    Update Odometer.java

--- Odometry2020SS/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Odometry/Odometer.java
+++ Odometry2020SS/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Odometry/Odometer.java
@@ -18,14 +18,21 @@ public class Odometer extends Subsystem{
     //Optical encoders
     private DcMotor rightEnc;
     private DcMotor leftEnc;
+    private DcMotor backEnc;
     //Important variables
-    private double robotRad;
+    private double robotCir;
     private double encdrRad;
+    private final double ticksPerCM = 500;
+    private double encScale;
 
     private double x;
     private double y;
     private double heading;
     private double[] position = {0, 0};
+
+    private double right;
+    private double left;
+    private double back;
 
     private double rightLastVal;
     private double leftLastVal;
@@ -35,7 +42,10 @@ public class Odometer extends Subsystem{
     private double leftChange;
     private double backChange;
 
-    private final double ticksPerCM = 500;
+    private double xOffestLR;
+
+    private double[] posChangeLR = new double[2];
+    private double[] posChangeB = new double[2];
 
     private boolean isRunning;
 
@@ -47,12 +57,13 @@ public class Odometer extends Subsystem{
         //IDK if this feature will be used, might be a pain
     }
 
-    public Odometer(DcMotor rightEncoder, DcMotor leftEncoder, double botRadius, double encRadius){
+    public Odometer(DcMotor rightEncoder, DcMotor leftEncoder, DcMotor backEncoder, double botRadius, double encRadius){
 
         this.rightEnc = rightEncoder;
         this.leftEnc = leftEncoder;
+        this.backEnc = backEncoder;
         this.encdrRad = encRadius;
-        this.robotRad = botRadius;
+        this.robotCir = 2*Math.PI*botRadius;
 
     }
 
@@ -62,13 +73,51 @@ public class Odometer extends Subsystem{
         y = 0;
         heading = 0;
 
+        encScale = encdrRad*2*Math.PI/ticksPerCM;
+
+        right = 0;
+        left = 0;
+        back = 0;
+
+        rightChange = 0;
+        leftChange = 0;
+        backChange = 0;
+
+        rightLastVal = 0;
+        leftLastVal = 0;
+        backLastVal = 0;
+
         rightEnc.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         rightEnc.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         leftEnc.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         leftEnc.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backEnc.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        backEnc.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
     }
 
     public void updateOdometry(){
+
+        right = rightEnc.getCurrentPosition() * encScale;
+        left = leftEnc.getCurrentPosition() * encScale;
+        back = backEnc.getCurrentPosition() * encScale;
+
+        heading = (left - right)/2/robotCir*360;
+
+        rightChange = right - rightLastVal;
+        leftChange = left - leftLastVal;
+        backChange = back - backLastVal;
+
+        if(rightChange == leftChange) {
+            posChangeLR[0] = 0;
+            posChangeLR[1] = rightChange;
+        }else{
+
+        }
+
+        rightLastVal = right;
+        leftLastVal = left;
+        backLastVal = back;
 
     }
 
